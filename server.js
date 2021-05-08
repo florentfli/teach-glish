dc2a4a64cc079c12fdd2fd3b3eba1a4d60f722c4
Florent FLIEDNER <florent.fli@gmail.com>
2021-05-08 16:54:11 +0200

    fix: serve static files

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 80;
 app.listen(port);
